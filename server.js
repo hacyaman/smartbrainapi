1109736758d1b804fab74435d0d8349412cfc826
unknown <hacya2015@hacya.com>
2020-05-24 10:31:24 +0900

    Update database url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-solid-86486',
       user : 'postgres',
       password : 'postgres',
       database : 'smart-brain'
